c8b9549aad5ac16ba9a58999f7f379e77b203cc3
salman-codebird <76891649+salman-codebird@users.noreply.github.com>
2021-04-07 21:29:14 +0530

    Update TestGreeter.java

--- src/test/java/com/example/TestGreeter.java
+++ src/test/java/com/example/TestGreeter.java
@@ -37,4 +37,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+  
+  @Test
+  public void greetShouldIncludeGreetingPhrase() {
+    String someone = "hello salman";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
